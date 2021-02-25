c19f60eb2fa6ba9a9df89530c312778f6adcf007
Marek Łukasiewicz <marek@lukasiewicz.tech>
2021-02-25 11:30:22 +0100

    Scheduling with alarms

--- background-script.js
+++ background-script.js
@@ -1,16 +1,33 @@
-function playSound() {
-  const soundUrl = browser.extension.getURL("sounds/single.mp3");
-  const iconUrl = browser.extension.getURL("icons/icon.png");
+function handleBellTime() {
+  const iconUrl = browser.extension.getURL("icons/bell-96.png");
   var element = document.getElementById("audioSingle");
   element.play();
   browser.notifications.create({
     "type": "basic",
     "iconUrl": iconUrl,
     "title": "Szklanka",
-    "message": `Element audio: ${element}`
+    "message": "Wybijanie jednej szklanki"
   });
 }
 
-// playSound();
-browser.alarms.onAlarm.addListener(playSound);
-browser.alarms.create('playSound', {periodInMinutes: 0.25});
+function scheduleBells() {
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  if (minutes >= 30) {
+    date.setHours(hours + 1);
+    date.setMinutes(0);
+  } else {
+    date.setMinutes(30);
+  }
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+
+  browser.alarms.onAlarm.addListener(handleBellTime);
+  browser.alarms.create('handleBellTime', {
+    when: date.getTime(),
+    periodInMinutes: 30
+  });
+}
+
+scheduleBells();
