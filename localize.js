7d84e5b9718a2734c06910e208d27d606fbe719f
Marek Łukasiewicz <marek@lukasiewicz.tech>
2021-03-02 14:23:45 +0100

    Fix unsafe innerHTML assignment

--- localize.js
+++ localize.js
@@ -3,7 +3,7 @@ let messagesTemplate = ''
 document.querySelectorAll('[data-i18n-msg]').forEach((node) => {
   let message = browser.i18n.getMessage(node.dataset.i18nMsg)
   if (message.length > 0) {
-    node.innerHTML = message
+    node.textContent = message
   } else {
     messagesTemplate += `,
 
